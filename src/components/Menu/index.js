55e2a068b6bf3a7fd0dc81dfd6e803d22e625da5
andrelake <andre.lake@hotmail.com>
2020-07-28 12:40:02 -0300

    Layout básico das páginas de cadastro

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './menu.css';
-import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="jazzflix logo" />
-      </a>
-      <ButtonLink className="ButtonLink" href="/">
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
-      </ButtonLink>
+      </Button>
     </nav>
   );
 }
