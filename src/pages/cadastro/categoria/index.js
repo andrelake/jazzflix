7a813e3879f08bb540b415d5f769cdb007739d19
andrelake <andre.lake@hotmail.com>
2020-07-29 13:18:55 -0300

    Upgrade formulario

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
+  }
+
+  function handleOnChange(event) {
+    setValue(event.target.getAttribute('name'), event.target.value);
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
 
+      <form
+        onSubmit={function handleSubmit(event) {
+          event.preventDefault();
+          setCategorias([...categorias, values]);
+
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria: "
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleOnChange}
+        />
+
+        <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleOnChange}
+            ></textarea>
+          </label>
+        </div>
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleOnChange}
+        />
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, index) => {
+          return <li key={`${categoria}${index}`}>{categoria.nome}</li>;
+        })}
+      </ul>
       <Link to="/">Ir para home</Link>
     </PageDefault>
   );
