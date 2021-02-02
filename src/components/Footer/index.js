98ba47b69d42a0f589c9460874713577de7984fc
André Lago <andre.lake@hotmail.com>
2021-02-02 20:57:22 -0300

    edit footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,11 +8,6 @@ function Footer() {
         src="http://localhost:3000/static/media/Logo.155cf848.png"
         alt="Logo JazzFlix"
       />
-
-      <p>
-        Orgulhosamente criado durante a{' '}
-        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
-      </p>
     </FooterBase>
   );
 }
